e66b60a509ee24d48453ee538f01ffa5df4f0459
olaulau <olaulau@gmail.com>
2014-11-25 01:41:49 +0100

    test and include php-i18n (https://github.com/Philipp15b/php-i18n) into project

---
+++ includes/trad.inc.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'ext/i18n.class.php';
+$i18n = new i18n(__DIR__ . '/../lang/lang_{LANGUAGE}.ini', __DIR__ . '/../lang/cache/', 'en');
+$i18n->init();
+?>
---
+++ test/test_trad.php
@@ -0,0 +1,6 @@
+<?php
+require_once './../includes/trad.inc.php';
+?>
+
+<?php echo L::greeting; ?></p>
+<?php echo L::category_somethingother; ?></p>
