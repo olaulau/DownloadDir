eeec7a281be7f8e9f54b8677eb695f2df19a01db
laulau <olaulau@gmail.com>
2018-07-30 21:58:31 +0200

    can delete symlink

--- actions/delete.get.php
+++ actions/delete.get.php
@@ -3,28 +3,32 @@ require_once '../includes/ALL.inc.php';
 
 Session::start();
 
-
 if(isset($_SESSION["user"])) {
 	if(!empty($_GET["file"])) {
 		$full_file = $base_dir."/" . $_GET["subdir"] . "/" . $_GET["file"];
-		if(file_exists($full_file)) {
-			if(!is_link($full_file) && is_dir($full_file)) {
-				$res = rmdir($full_file);
-			}
-			else {
-				$res = unlink($full_file);
-			}
-// 			echo ($res ? "OK !" : "KO :-("); die;
+		if (is_link($full_file)) {
+			exec('rm -f "' . $full_file . '"', $output, $return_var);
 			redirect();
 		}
 		else {
-			echo L::actions_doesnt_exist;
+			if(file_exists($full_file)) {
+				if(!is_link($full_file) && is_dir($full_file)) {
+					$res = rmdir($full_file);
+				}
+				else {
+					$res = unlink($full_file);
+				}
+	// 			echo ($res ? "OK !" : "KO :-("); die;
+				redirect();
+			}
+			else {
+				echo L::actions_doesnt_exist;
+			}
 		}
 	}
 	else {
 		echo L::actions_parameter_problem . '.';
 	}
-	
 }
 else {
 	sleep(3);
